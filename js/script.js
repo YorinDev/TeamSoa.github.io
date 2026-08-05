588b7a26016c14baa6e52d40509071c4fe48cafa
Yorin <vincentburel.86@gmail.com>
2026-08-05 23:13:53 +0200

    Refactor Firebase setup and add authentication
    Updated Firebase imports to use CDN links and added authentication functionality.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,47 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAuAMnfhhn3so9FV70mktoBqhM_wZrQY_k",
-  authDomain: "soaesport-85eff.firebaseapp.com",
-  projectId: "soaesport-85eff",
-  storageBucket: "soaesport-85eff.firebasestorage.app",
-  messagingSenderId: "515591931528",
-  appId: "1:515591931528:web:cc13849028210f50f8413d",
-  measurementId: "G-LHL50FX5KD"
+    apiKey: "AIzaSyAuAMnfhhn3so9FV70mktoBqhM_wZrQY_k",
+    authDomain: "soaesport-85eff.firebaseapp.com",
+    projectId: "soaesport-85eff",
+    storageBucket: "soaesport-85eff.firebasestorage.app",
+    messagingSenderId: "515591931528",
+    appId: "1:515591931528:web:cc13849028210f50f8413d",
+    measurementId: "G-LHL50FX5KD"
 };
 
-// Initialize Firebase
+// Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialisation de l'authentification
+const auth = getAuth(app);
+
+// Rend l'auth accessible depuis d'autres scripts (facultatif)
+window.auth = auth;
+
+// Vérifie si un utilisateur est connecté
+onAuthStateChanged(auth, (user) => {
+
+    if (user) {
+        console.log("Utilisateur connecté :", user.email);
+    } else {
+        console.log("Aucun utilisateur connecté");
+    }
+
+});
+
+// Exporte les fonctions utiles
+export {
+    auth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
+};
